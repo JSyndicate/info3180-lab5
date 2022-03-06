860c9779733f422409b6e3d581074504579fdbf8
JSyndicate <jevwilliamson@gmail.com>
2022-03-06 16:05:57 -0500

    your message here

--- app/views.py
+++ app/views.py
@@ -80,6 +80,14 @@ def login():
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You were been logged out.')
+    return redirect(url_for('home'))
+
+
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
